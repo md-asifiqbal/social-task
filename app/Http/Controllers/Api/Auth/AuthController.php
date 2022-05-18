0cd97c4b5855b44961b508234c1d670ac223dcb8
asif <asif.ice.pust@gmail.com>
2022-05-19 01:07:39 +0600

    Login function added

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
 class AuthController extends Controller
 {
@@ -12,9 +13,25 @@ class AuthController extends Controller
     //User Registration
     public function register(RegisterRequest $request)
     {
-      //  dd($request->validated());
-       $data=User::create($request->validated());
-       $this->apiSuccess(__("User Registration Successfuly"),$data);
+        $input=$request->validated();
+        $input['password']=bcrypt($request->password);
+       $user=User::create($input);
+       $this->apiSuccess(__("User Registration Successfuly"),$user);
+       return $this->apiOutput();
+    }
+
+    //User Registration
+    public function login(LoginRequest $request)
+    {
+       $user=User::where('email',$request->email)->first();
+       if($user){
+        $this->api_token=$user->createToken($user->email)->plainTextToken;
+        $this->apiSuccess(__("User Login Successfuly"),$user);
+       }else{
+        $this->status=false;
+        $this->message="Email or Password may be wrong";
+       }
+       
        return $this->apiOutput();
     }
 }
--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class RegisterRequest extends FormRequest
+class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,12 +24,8 @@ class RegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            'first_name'=>'required|string|max:255',
-            'last_name'=>'required|string|max:255',
-            'email'=>'required|email|max:255|unique:users',
-            'password'=>'required|min:8|confirmed',
+            'email'=>'required|email',
+            'password'=>'required',
         ];
     }
-
-   
 }
